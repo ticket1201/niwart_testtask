799da0800d02c6b489838652d8960b0e7b9b8ad3
Anton Metlitsky <ticket1201@gmail.com>
2023-02-20 19:21:59 +0400

    feat: add components to App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ArticleWrapper} from "./components/ArticleWrapper/ArticleWrapper";
+import {VisaAI} from "./components/VisaAI/VisaAI";
+import {VisaAIHow} from "./components/VisaAIHow/VisaAIHow";
+import {Blog} from "./components/Blog/Blog";
+import {VisaAIHowSlider} from "./components/VisaAIHow/VisaAIHowSlider";
+import {VisaAISlider} from "./components/VisaAI/VisaAISlider";
+import {BlogSlider} from "./components/Blog/BlogSlider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <ArticleWrapper titleText={'Visas.AI'}
+                            subTitleText={'The only platform that will make you a better attorney with better visa approval ratings.'}
+            >
+                <VisaAI/>
+                <VisaAISlider/>
+            </ArticleWrapper>
+            <ArticleWrapper titleText={'How Visas.AI works for You'}
+                            subTitleText={'The only platform that will make you a better attorney with better visa approval ratings.'}
+            >
+                <>
+                    <VisaAIHow/>
+                    <VisaAIHowSlider/>
+                </>
+            </ArticleWrapper>
+            <ArticleWrapper titleText={'Blog'}>
+                <>
+                    <Blog/>
+                    <BlogSlider/>
+                </>
+            </ArticleWrapper>
+        </main>
+    );
 }
 
 export default App;
